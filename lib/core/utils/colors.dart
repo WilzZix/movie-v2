3af4b2614a6ed0c991ed3cd7af9a3458f8803be5
nodirbekbarotov <nodirbek.barotov@opentech.uz>
2025-02-17 14:15:40 +0500

    Typographies and colors added

--- lib/core/utils/colors.dart
+++ lib/core/utils/colors.dart
@@ -5,3 +5,71 @@ class AppColors {
   static Color tabletCardColor = const Color(0xFF81817E);
   static Color tabletHeaderColor = const Color(0xFF868584);
 }
+
+class MainPrimaryColor {
+  static Color primary500 = const Color(0xFF1b9ceb);
+  static Color primary400 = const Color(0xFF3ba9ed);
+  static Color primary300 = const Color(0xFF6abdf1);
+  static Color primary200 = const Color(0xFFa0d1f0);
+  static Color primary100 = const Color(0xFFe9f5fd);
+}
+
+class SecondaryPrimaryColor {
+  static Color primary500 = const Color(0xFFff0000);
+  static Color primary400 = const Color(0xFFff3333);
+  static Color primary300 = const Color(0xFFff6666);
+  static Color primary200 = const Color(0xFFff9999);
+  static Color primary100 = const Color(0xFFffe6e5);
+}
+
+class GreyScale {
+  static Color grayScale900 = const Color(0xFF212121);
+  static Color grayScale800 = const Color(0xFF424242);
+  static Color grayScale700 = const Color(0xFF616161);
+  static Color grayScale600 = const Color(0xFF757575);
+  static Color grayScale500 = const Color(0xFF9E9E9E);
+  static Color grayScale400 = const Color(0xFFBDBDBD);
+  static Color grayScale300 = const Color(0xFFe0e0e0);
+  static Color grayScale200 = const Color(0xFFEEEEEE);
+  static Color grayScale100 = const Color(0xFFf5f5f5);
+  static Color grayScale50 = const Color(0xFFfafafa);
+}
+
+class DarkColors {
+  static Color dark1 = const Color(0xFF121A20);
+  static Color dark2 = const Color(0xFF1F222A);
+  static Color dark3 = const Color(0xFF35383F);
+}
+
+class OtherColors {
+  static Color white = const Color(0xFFFFFFFF);
+  static Color black = const Color(0xFF000000);
+  static Color rad = const Color(0xFFF44336);
+  static Color pink = const Color(0xFFE91E63);
+  static Color purple = const Color(0xFF9C27B0);
+  static Color deepPurple = const Color(0xFF673AB7);
+  static Color indigo = const Color(0xFF3F51B5);
+  static Color blue = const Color(0xFF2196F3);
+  static Color lightBlue = const Color(0xFF03A9F4);
+  static Color cyan = const Color(0xFF00BCD4);
+  static Color teal = const Color(0xFF009688);
+  static Color green = const Color(0xFF4CAF50);
+  static Color lightGreen = const Color(0xFF8BC34A);
+  static Color lime = const Color(0xFFCDDC39);
+  static Color yellow = const Color(0xFFFFEB3B);
+  static Color amber = const Color(0xFFFFC107);
+  static Color orange = const Color(0xFFFF9800);
+  static Color deppOrange = const Color(0xFFFF5722);
+  static Color brown = const Color(0xFF795548);
+  static Color blueGray = const Color(0xFF607D8B);
+}
+
+class Transparent {
+  static Color red = const Color(0xFFfdeced);
+  static Color purple = const Color(0xFFf5ecff);
+  static Color blue = const Color(0xFFedf3ff);
+  static Color orange = const Color(0xFFfff7eb);
+  static Color yellow = const Color(0xFFfffbeb);
+  static Color green = const Color(0xFFf2f9f1);
+  static Color cyan = const Color(0xFFebfafc);
+}
--- lib/core/utils/typography.dart
+++ lib/core/utils/typography.dart
@@ -1,1 +1,127 @@
-class Typographies {}
+import 'dart:ui';
+
+import 'package:movie/core/utils/colors.dart';
+
+class Typographies {
+  ///Heading
+  static TextStyle heading1 = TextStyle(
+    fontWeight: FontWeight.bold,
+    color: GreyScale.grayScale900,
+    height: 48,
+  );
+  static TextStyle heading2 = TextStyle(
+    fontWeight: FontWeight.bold,
+    color: GreyScale.grayScale900,
+    height: 40,
+  );
+  static TextStyle heading3 = TextStyle(
+    fontWeight: FontWeight.bold,
+    color: GreyScale.grayScale900,
+    height: 32,
+  );
+  static TextStyle heading4 = TextStyle(
+    fontWeight: FontWeight.bold,
+    color: GreyScale.grayScale900,
+    height: 24,
+  );
+  static TextStyle heading5 = TextStyle(
+    fontWeight: FontWeight.bold,
+    color: GreyScale.grayScale900,
+    height: 20,
+  );
+  static TextStyle heading6 = TextStyle(
+    fontWeight: FontWeight.bold,
+    color: GreyScale.grayScale900,
+    height: 18,
+  );
+
+  ///Body XLarge
+  static TextStyle bodyXLargeBold = TextStyle(
+    height: 18,
+    fontWeight: FontWeight.bold,
+  );
+  static TextStyle bodyXLargeSemiBold = TextStyle(
+    height: 18,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle bodyXLargeMedium = TextStyle(
+    height: 18,
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle bodyXLargeRegular = TextStyle(
+    height: 18,
+    fontWeight: FontWeight.w400,
+  );
+
+  ///Body Large
+  static TextStyle bodyLargeBold = TextStyle(
+    height: 16,
+    fontWeight: FontWeight.bold,
+  );
+  static TextStyle bodyLargeSemiBold = TextStyle(
+    height: 16,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle bodyLargeMedium = TextStyle(
+    height: 16,
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle bodyLargeRegular = TextStyle(
+    height: 16,
+    fontWeight: FontWeight.w400,
+  );
+
+  ///Body Medium
+  static TextStyle bodyMediumBold = TextStyle(
+    height: 14,
+    fontWeight: FontWeight.bold,
+  );
+  static TextStyle bodyMediumSemiBold = TextStyle(
+    height: 14,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle bodyMediumMedium = TextStyle(
+    height: 14,
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle bodyMediumRegular = TextStyle(
+    height: 14,
+    fontWeight: FontWeight.w400,
+  );
+
+  ///Body Small
+  static TextStyle bodySmallBold = TextStyle(
+    height: 12,
+    fontWeight: FontWeight.bold,
+  );
+  static TextStyle bodySmallSemiBold = TextStyle(
+    height: 12,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle bodySmallMedium = TextStyle(
+    height: 12,
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle bodySmallRegular = TextStyle(
+    height: 12,
+    fontWeight: FontWeight.w400,
+  );
+
+  ///Body XSmall
+  static TextStyle bodyXSmallBold = TextStyle(
+    height: 10,
+    fontWeight: FontWeight.bold,
+  );
+  static TextStyle bodyXSmallSemiBold = TextStyle(
+    height: 10,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle bodyXSmallMedium = TextStyle(
+    height: 10,
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle bodyXSmallRegular = TextStyle(
+    height: 10,
+    fontWeight: FontWeight.w400,
+  );
+}
