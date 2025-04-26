4a6a29269cdd65776f1e9d191f5a80cf27e190f7
WilzZix <nodirbarotov0707@gmail.com>
2025-04-26 08:47:28 +0500

    stack removed

--- lib/presentation/pages/on_boarding/login_and_registration_page.dart
+++ lib/presentation/pages/on_boarding/login_and_registration_page.dart
@@ -22,92 +22,77 @@ class _LoginAndRegistrationPageState extends State<LoginAndRegistrationPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Image.asset(
-            'assets/icons/garfild_icon.png',
-            fit: BoxFit.fill,
-          ),
-          BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 8.0, sigmaY: 8.0),
-            child: const SizedBox(
-              width: 200,
-              height: 200,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).padding.top,
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Column(
-              children: [
-                SizedBox(
-                  height:MediaQuery.of(context).padding.top,
-                ),
-                AppIcons.icAppIcon,
-                const SizedBox(height: 32),
-                Text(
-                  'Start Streaming Now with Ava',
-                  textAlign: TextAlign.center,
-                  style: Typographies.heading2.copyWith(color: Colors.white),
-                ),
-                const SizedBox(height: 60),
-                const Buttons.social(type3: ButtonType3.google),
-                const SizedBox(height: 16),
-                const Buttons.social(type3: ButtonType3.apple),
-                const SizedBox(height: 24),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      Expanded(
-                        child: Divider(
-                          height: 1,
-                          color: GreyScale.grayScale200,
-                        ),
-                      ),
-                      const SizedBox(width: 16),
-                      const Text('or'),
-                      const SizedBox(width: 16),
-                      Expanded(
-                        child: Divider(
-                          height: 1,
-                          color: GreyScale.grayScale200,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 24),
-                Buttons.primary(
-                  text: 'Sign in',
-                  onTap: () {},
-                ),
-                const SizedBox(height: 60),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Don’t have an account?',
-                      style: Typographies.bodyMediumRegular
-                          .copyWith(color: GreyScale.grayScale500),
+            AppIcons.icAppIcon,
+            const SizedBox(height: 32),
+            Text(
+              'Start Streaming Now with Ava',
+              textAlign: TextAlign.center,
+              style: Typographies.heading2,
+            ),
+            const SizedBox(height: 60),
+            const Buttons.social(type3: ButtonType3.google),
+            const SizedBox(height: 16),
+            const Buttons.social(type3: ButtonType3.apple),
+            const SizedBox(height: 24),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Expanded(
+                    child: Divider(
+                      height: 1,
+                      color: GreyScale.grayScale200,
                     ),
-                    const SizedBox(width: 8),
-                    GestureDetector(
-                      onTap: () {
-                        context.pushNamed(SignUpPage.tag);
-                      },
-                      child: Text(
-                        'Sign up',
-                        style: Typographies.bodyMediumSemiBold
-                            .copyWith(color: MainPrimaryColor.primary500),
-                      ),
-                    )
-                  ],
+                  ),
+                  const SizedBox(width: 16),
+                  const Text('or'),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: Divider(
+                      height: 1,
+                      color: GreyScale.grayScale200,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 24),
+            Buttons.primary(
+              text: 'Sign in',
+              onTap: () {},
+            ),
+            const SizedBox(height: 60),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Don’t have an account?',
+                  style: Typographies.bodyMediumRegular
+                      .copyWith(color: GreyScale.grayScale500),
                 ),
+                const SizedBox(width: 8),
+                GestureDetector(
+                  onTap: () {
+                    context.pushNamed(SignUpPage.tag);
+                  },
+                  child: Text(
+                    'Sign up',
+                    style: Typographies.bodyMediumSemiBold
+                        .copyWith(color: MainPrimaryColor.primary500),
+                  ),
+                )
               ],
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/presentation/pages/on_boarding/sign_up_page.dart
+++ lib/presentation/pages/on_boarding/sign_up_page.dart
@@ -1,12 +1,14 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:movie/core/utils/colors.dart';
 import 'package:movie/core/utils/components/button.dart';
 import 'package:movie/core/utils/components/checkbox.dart';
 import 'package:movie/core/utils/components/inputs/input_fields.dart';
 import 'package:movie/core/utils/icons/icons.dart';
 import 'package:movie/core/utils/typography.dart' show Typographies;
+import 'package:movie/presentation/pages/bottom_navigation/bottom_navigation.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
@@ -37,155 +39,149 @@ class _SignUpPageState extends State<SignUpPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Stack(children: [
-        Image.asset(
-          'assets/icons/garfild_icon.png',
-          fit: BoxFit.fill,
-        ),
-        BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 8.0, sigmaY: 8.0),
-          child: const SizedBox(
-            width: 200,
-            height: 200,
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: MediaQuery.of(context).padding.top,
-                ),
-                AppIcons.icAppIcon,
-                const SizedBox(height: 32),
-                Text(
-                  'Login to Your Account',
-                  textAlign: TextAlign.center,
-                  style: Typographies.heading2.copyWith(color: Colors.white),
-                ),
-                const SizedBox(height: 100),
-                InputField.email(
-                  hintText: 'email',
-                  focusNode: focusNodeEmail,
-                  controller: controllerEmail,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                InputField.password(
-                  hintText: 'Password',
-                  focusNode: focusNodePassword,
-                  controller: controllerPassword,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Row(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).padding.top,
+              ),
+              AppIcons.icAppIcon,
+              const SizedBox(height: 32),
+              Text(
+                'Login to Your Account',
+                textAlign: TextAlign.center,
+                style: Typographies.heading2,
+              ),
+              const SizedBox(height: 100),
+              InputField.email(
+                hintText: 'email',
+                focusNode: focusNodeEmail,
+                controller: controllerEmail,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              InputField.password(
+                hintText: 'Password',
+                focusNode: focusNodePassword,
+                controller: controllerPassword,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const AppCheckbox(),
+                  const SizedBox(width: 8),
+                  Text(
+                    'Remember me',
+                    style: Typographies.bodyMediumSemiBold,
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Buttons.primary(
+                text: 'Sign up',
+                onTap: () {
+                  context.pushNamed(BottomNavigationPage.tag);
+                },
+              ),
+              const SizedBox(height: 37),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
                   children: [
-                    const AppCheckbox(),
-                    const SizedBox(width: 8),
-                    Text(
-                      'Remember me',
-                      style: Typographies.bodyMediumSemiBold,
-                    )
+                    Expanded(
+                      child: Divider(
+                        height: 1,
+                        color: GreyScale.grayScale200,
+                      ),
+                    ),
+                    const SizedBox(width: 16),
+                    const Text('or continue with'),
+                    const SizedBox(width: 16),
+                    Expanded(
+                      child: Divider(
+                        height: 1,
+                        color: GreyScale.grayScale200,
+                      ),
+                    ),
                   ],
                 ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const Buttons.primary(text: 'Sign up'),
-                const SizedBox(height: 37),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      Expanded(
-                        child: Divider(
-                          height: 1,
-                          color: GreyScale.grayScale200,
-                        ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 60,
+                    width: 87,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      border: Border.all(
+                        color: GreyScale.grayScale200,
                       ),
-                      const SizedBox(width: 16),
-                      const Text('or continue with'),
-                      const SizedBox(width: 16),
-                      Expanded(
-                        child: Divider(
-                          height: 1,
-                          color: GreyScale.grayScale200,
-                        ),
-                      ),
-                    ],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          top: 18.0, bottom: 18, right: 32, left: 32),
+                      child: AppIcons.icGoogle,
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  children: [
-                    Container(
-                      height: 60,
-                      width: 87,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(16),
-                        border: Border.all(
-                          color: GreyScale.grayScale200,
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.only(
-                            top: 18.0, bottom: 18, right: 32, left: 32),
-                        child: AppIcons.icGoogle,
+                  const SizedBox(width: 20),
+                  Container(
+                    height: 60,
+                    width: 87,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      border: Border.all(
+                        color: GreyScale.grayScale200,
                       ),
                     ),
-                    const SizedBox(width: 20),
-                    Container(
-                      height: 60,
-                      width: 87,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(16),
-                        border: Border.all(
-                          color: GreyScale.grayScale200,
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.only(
-                            top: 18.0, bottom: 18, right: 32, left: 32),
-                        child: AppIcons.icApple,
-                      ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          top: 18.0, bottom: 18, right: 32, left: 32),
+                      child: AppIcons.icApple,
                     ),
-                  ],
-                ),
-                const SizedBox(height: 37),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Already have an account?',
-                      style: Typographies.bodyMediumRegular
-                          .copyWith(color: GreyScale.grayScale500),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 37),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Already have an account?',
+                    style: Typographies.bodyMediumRegular
+                        .copyWith(color: GreyScale.grayScale500),
+                  ),
+                  const SizedBox(width: 8),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                      'Sign in',
+                      style: Typographies.bodyMediumSemiBold
+                          .copyWith(color: MainPrimaryColor.primary500),
                     ),
-                    const SizedBox(width: 8),
-                    GestureDetector(
-                      onTap: () {},
-                      child: Text(
-                        'Sign in',
-                        style: Typographies.bodyMediumSemiBold
-                            .copyWith(color: MainPrimaryColor.primary500),
-                      ),
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).padding.bottom,
-                )
-              ],
-            ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).padding.bottom,
+              )
+            ],
           ),
-        )
-      ]),
+        ),
+      ),
     );
   }
 }
