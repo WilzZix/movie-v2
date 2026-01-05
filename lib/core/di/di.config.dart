dff084197c65e74ec8d2f75ea00310b1323d006f
wilzix <nodirbarotov0707@gmail.com>
2026-01-05 11:24:11 +0500

    Revert "debounce added"
    This reverts commit 8d59714f9fbde39aee0b0831214eca4237592627.

--- lib/core/di/di.config.dart
+++ lib/core/di/di.config.dart
@@ -1,5 +1,4 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
-// dart format width=80
 
 // **************************************************************************
 // InjectableConfigGenerator
@@ -9,10 +8,8 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:dio/dio.dart' as _i361;
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
-import 'package:movie/core/dio/dio_provider.dart' as _i500;
 import 'package:movie/data/datasources/local_data_source/shared_preference_service.dart'
     as _i1066;
 import 'package:movie/data/datasources/network_data_source/firebase_auth.dart'
@@ -21,8 +18,6 @@ import 'package:movie/data/datasources/network_data_source/moviedb.dart'
     as _i1043;
 import 'package:movie/data/datasources/network_data_source/network_movies_datasource.dart'
     as _i549;
-import 'package:movie/data/movie_api/movie_api.dart' as _i756;
-import 'package:movie/data/network_movie_api/network_movie_api.dart' as _i667;
 import 'package:movie/domain/repositories/i_firebase_auth.dart' as _i1053;
 import 'package:movie/domain/repositories/i_movie_auth.dart' as _i734;
 import 'package:movie/domain/repositories/i_movies_repository.dart' as _i589;
@@ -38,21 +33,12 @@ extension GetItInjectableX on _i174.GetIt {
       environment,
       environmentFilter,
     );
-    final dioProvider = _$DioProvider();
-    gh.singletonAsync<_i361.Dio>(() => dioProvider.getAuthorizedDioClient());
     gh.singleton<_i1066.SharedPreferenceService>(
         () => _i1066.SharedPreferenceService());
     gh.singleton<_i1053.IFirebaseAuth>(() => _i819.FirebaseAuthImpl());
-    gh.singletonAsync<_i756.MovieApi>(
-        () async => _i756.MovieApi(await getAsync<_i361.Dio>()));
-    gh.singletonAsync<_i667.NetworkMovieApi>(
-        () async => _i667.NetworkMovieApi(await getAsync<_i361.Dio>()));
-    gh.singletonAsync<_i734.MovieDBAuth>(() async =>
-        _i1043.FirebaseAuthRepository(await getAsync<_i756.MovieApi>()));
-    gh.singletonAsync<_i589.IMoviesRepository>(() async =>
-        _i549.NetworkMoviesDataSource(await getAsync<_i667.NetworkMovieApi>()));
+    gh.singleton<_i734.MovieDBAuth>(() => _i1043.FirebaseAuthRepository());
+    gh.singleton<_i589.IMoviesRepository>(
+        () => _i549.NetworkMoviesDataSource());
     return this;
   }
 }
-
-class _$DioProvider extends _i500.DioProvider {}
